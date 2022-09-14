72cac29ffd6dd60566a924d628dfa403ee2f8d8b
Isabela Marques <88113608+isamarques@users.noreply.github.com>
2022-09-13 22:06:42 -0300

    Update Principal.java

--- view/Principal.java
+++ view/Principal.java
@@ -1,14 +1,22 @@
 package view;
-
+import java.util.Random;
 import controller.Processador;
 
 public class Principal {
 
 	public static void main(String[] args) {
 	
+		Random random = new Random();
+		
 		Processador proc = new Processador();
+		int[] prato = new int[5];
 	
 		proc.iniciaPrato();
+		
+		for (int i=0; i<prato.length; i++) {
+			prato[i] = 1 + random.nextInt(90); 
+            System.out.println(prato[i]); 
+       }
 	}
 
 }
